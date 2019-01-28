e1081205c2d77d5b29f26d5a0bd7bd2021d17666
Jesco Wuester <jescowuester@gmail.com>
2019-01-28 11:16:10 +0100

    Updates

---
+++ precache-manifest.c10c43f49b20790384f1f519268ea2db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f167fbeae46678ab85e4",
+    "url": "/routing-react-gh-pages/static/css/main.a9da1fdf.chunk.css"
+  },
+  {
+    "revision": "f167fbeae46678ab85e4",
+    "url": "/routing-react-gh-pages/static/js/main.f167fbea.chunk.js"
+  },
+  {
+    "revision": "f06e0c25a89ead78e14b",
+    "url": "/routing-react-gh-pages/static/js/1.f06e0c25.chunk.js"
+  },
+  {
+    "revision": "06df1bbeea9601b7194b",
+    "url": "/routing-react-gh-pages/static/js/runtime~main.06df1bbe.js"
+  },
+  {
+    "revision": "720bb03cfaa3cff02b931eaa75347311",
+    "url": "/routing-react-gh-pages/index.html"
+  }
+];
